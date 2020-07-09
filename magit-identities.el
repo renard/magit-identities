e0b14571d98b55e123cc40e43db3ef46a8837ba5
Sébastien Gross <sebchezwamorg>
2020-07-09 23:43:18 +0200

    Update to new magit versions
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- magit-identities.el
+++ magit-identities.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2012-08-30
-;; Last changed: 2013-04-11 20:54:42
+;; Last changed: 2020-07-09 23:43:53
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -49,23 +49,26 @@ Order matters: first matched identity is used."
    remote-branch@remote::remote-url"
   (let* ((branch (magit-get-current-branch))
 	 (remote (and branch (magit-get "branch" branch "remote")))
-	 (remote-branch (or (and branch (magit-remote-branch-for branch)) branch))
+	 (remote-branch
+	  (or (and branch
+		   (substring-no-properties (magit-get-upstream-branch branch)))
+	      branch))
 	 (remote-url (and remote (magit-get "remote" remote "url"))))
     (format "%s@%s::%s" remote-branch remote remote-url)))
 
 (defun magit-identities-get-id (repository)
-  "Give REPOSITORY return a matching id from
+  "Given REPOSITORY return a matching id from
 `magit-identities-alist'."
-  (loop for (id regexp fields) in magit-identities-alist
+  (loop for (id regexp name email) in magit-identities-alist
 	when (string-match-p regexp repository)
-	return fields))
+	return id))
 
 ;;;###autoload
-(defun magit-identities-set-id()
+(defun magit-identities-set-default-id()
   "Set default identity for current checkout."
-  (magit-log-edit-set-fields
-   (magit-identities-get-id
-    (magit-identities-get-repo-string))))
+  (magit-identities-change
+   (symbol-name (magit-identities-get-id
+		 (magit-identities-get-repo-string)))))
 
 ;;;###autoload
 (defun magit-identities-change(id)
@@ -76,12 +79,24 @@ Order matters: first matched identity is used."
 	    (loop for i in magit-identities-alist
 		  collect (symbol-name (car i)))
 	    nil 1 )))
-    (magit-log-edit-set-fields
-     (caddr (assoc (intern id) magit-identities-alist))))
-(define-key magit-log-edit-mode-map (kbd "C-c C-p") 'magit-identities-change)
+    (apply #'magit-identities-set-id
+	   (cddr (assoc (intern id) magit-identities-alist))))
+
+(define-key magit-mode-map (kbd "C-c C-p") 'magit-identities-change)
 
 
-;;;###autoload
-(add-hook 'magit-log-edit-mode-hook 'magit-identities-set-id)
+;; ;;;###autoload
+;; (add-hook 'magit-log-edit-mode-hook 'magit-identities-set-id)
+
+(defun magit-identities-set-id (name email)
+  "Set identity using both NAME and EMAIL to the local repository."
+  (let ((cur-name (car (magit-git-lines "config" "user.name")))
+	(cur-email (car (magit-git-lines "config" "user.email"))))
+    ;;(message "'%s' '%s' | '%s' '%s'" name cur-name email cur-email)
+    (unless (equal cur-name name)
+      (magit-run-git "config" "--local" "--replace-all" "user.name" name))
+    (unless (equal cur-email email)
+      (magit-run-git "config" "--local" "--replace-all" "user.email" email))))
+
 
 ;; magit-identities.el ends here
